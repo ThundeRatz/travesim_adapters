92759e57720f6223981db1503c9c54b38a55ce2e
Felipe Gomes de Melo <felipegmelo.42@gmail.com>
2021-04-03 11:55:56 -0300

    🎨  Format vision_adapter.cpp

--- src/bin/vision_adapter.cpp
+++ src/bin/vision_adapter.cpp
@@ -15,29 +15,27 @@
 
 #include <iostream>
 
-int main(int argc, char** argv)
-{
-  int32_t send_rate;
-  VisionReceiver vision_receiver;
+int main(int argc, char** argv) {
+    int32_t send_rate;
+    VisionReceiver vision_receiver;
 
-  ros::init(argc, argv, "vision_adapter");
-  ros::NodeHandle nh;
+    ros::init(argc, argv, "vision_adapter");
+    ros::NodeHandle nh;
 
-  ros::Subscriber sub = nh.subscribe("/gazebo/model_states", 2, &VisionReceiver::receive, &vision_receiver);
+    ros::Subscriber sub = nh.subscribe("/gazebo/model_states", 2, &VisionReceiver::receive, &vision_receiver);
 
-  nh.param<int32_t>("send_rate", send_rate, 60);
+    nh.param<int32_t>("send_rate", send_rate, 60);
 
-  ros::Rate loop_rate(send_rate);
+    ros::Rate loop_rate(send_rate);
 
-  ROS_INFO_STREAM("Vision adapter started with loop rate " << send_rate);
+    ROS_INFO_STREAM("Vision adapter started with loop rate " << send_rate);
 
-  while (ros::ok())
-  {
-    // Send message with protobuf
-    ROS_INFO_STREAM(vision_receiver.world_state[0].model_name
-                    << " X: " << vision_receiver.world_state[0].pose.position.x
-                    << " Y: " << vision_receiver.world_state[0].pose.position.x);
-    ros::spinOnce();
-    loop_rate.sleep();
-  }
+    while (ros::ok()) {
+        // Send message with protobuf
+        ROS_INFO_STREAM(vision_receiver.world_state[0].model_name
+                        << " X: " << vision_receiver.world_state[0].pose.position.x
+                        << " Y: " << vision_receiver.world_state[0].pose.position.x);
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
 }
