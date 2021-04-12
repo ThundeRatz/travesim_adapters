b3d9e02ea04d9b2291450c4dfb326b358f557392
LucasHaug <lucas.haug@hotmail.com>
2021-04-11 23:49:31 -0300

    ✨ Add field state data

---
+++ include/travesim_adapters/data/field_state.hpp
@@ -0,0 +1,48 @@
+/**
+ * @file field_command.hpp
+ *
+ * @brief Field state data structure
+ *
+ * @author Lucas Haug <lucas.haug@thunderatz.org>
+ *
+ * @date 04/2021
+ */
+
+#ifndef __FIELD_STATE_H__
+#define __FIELD_STATE_H__
+
+#include <iostream>
+#include "travesim_adapters/data/data_constants.hpp"
+#include "travesim_adapters/data/entity_state.hpp"
+#include "travesim_adapters/data/robot_state.hpp"
+
+namespace travesim_adapters {
+    /**
+     * @brief Data structure to hold the field state
+     *
+     */
+    class FieldState {
+        public:
+            /**
+             * @brief Construct a new Field State object
+             *
+             */
+            FieldState();
+
+            /**
+             * @brief Output stream operator overloading
+             *
+             */
+            friend std::ostream& operator<<(std::ostream& output, const FieldState& field_state);
+
+            /**
+             * @brief Public attributes
+             *
+             */
+            EntityState ball;
+            RobotState yellow_team[NUM_OF_ROBOTS_PER_TEAM];
+            RobotState blue_team[NUM_OF_ROBOTS_PER_TEAM];
+    };
+}
+
+#endif  // __FIELD_STATE_H__
---
+++ src/lib/data/field_state.cpp
@@ -0,0 +1,47 @@
+/**
+ * @file team_command.cpp
+ *
+ * @brief Team command data structure
+ *
+ * @author Lucas Haug <lucas.haug@thunderatz.org>
+ *
+ * @date 04/2021
+ */
+
+#include <iomanip>
+#include "travesim_adapters/data/field_state.hpp"
+
+namespace travesim_adapters {
+    FieldState::FieldState() {
+        for (int i = 0; i < NUM_OF_ROBOTS_PER_TEAM; i++) {
+            this->yellow_team[i].id = i;
+            this->yellow_team[i].team_yellow = true;
+
+            this->blue_team[i].id = i;
+            this->blue_team[i].team_yellow = false;
+        }
+    }
+
+    std::ostream& operator<<(std::ostream& output, const FieldState& field_state) {
+        output << "BALL STATE: " << std::endl;
+        output << field_state.ball << std::endl;
+
+        output << "TEAM YELLOW STATE: " << std::endl;
+        for (int i = 0; i < NUM_OF_ROBOTS_PER_TEAM; i++) {
+            output << "ROBOT " << i << ":" << std::endl;
+            output << field_state.yellow_team[i] << std::endl;
+        }
+
+        output << std::endl;
+
+        output << "TEAM BLUE STATE: " << std::endl;
+        for (int i = 0; i < NUM_OF_ROBOTS_PER_TEAM; i++) {
+            output << "ROBOT " << i << ":" << std::endl;
+            output << field_state.blue_team[i] << std::endl;
+        }
+
+        output << std::endl;
+
+        return output;
+    }
+}
