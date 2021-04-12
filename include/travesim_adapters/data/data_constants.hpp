6cd94ae2d1c357ae558afada7b51272863fc3b8d
LucasHaug <lucas.haug@hotmail.com>
2021-04-11 21:29:51 -0300

    ✨ Add team command data

---
+++ include/travesim_adapters/data/data_constants.hpp
@@ -0,0 +1,16 @@
+/**
+ * @file data_constants.hpp
+ *
+ * @brief General constants for the data structures
+ *
+ * @author Lucas Haug <lucas.haug@thunderatz.org>
+ *
+ * @date 04/2021
+ */
+
+#ifndef __DATA_CONSTANTS_H__
+#define __DATA_CONSTANTS_H__
+
+#define NUM_OF_ROBOTS_PER_TEAM 3
+
+#endif // __DATA_CONSTANTS_H__
---
+++ src/lib/data/team_command.cpp
@@ -0,0 +1,37 @@
+/**
+ * @file team_command.cpp
+ *
+ * @brief Team command data structure
+ *
+ * @author Lucas Haug <lucas.haug@thunderatz.org>
+ *
+ * @date 04/2021
+ */
+
+#include <iomanip>
+#include "travesim_adapters/data/team_command.hpp"
+
+namespace travesim_adapters {
+    RobotCommand::RobotCommand(double left_speed, double right_speed) {
+        this->left_speed = left_speed;
+        this->right_speed = right_speed;
+    }
+
+    std::ostream& operator<<(std::ostream& output, const RobotCommand& command) {
+        output << "LEFT SPEED: " << std::setw(6) << command.left_speed << std::endl;
+        output << "RIGHT SPEED: " << std::setw(5) << command.right_speed;
+
+        return output;
+    }
+
+
+    std::ostream& operator<<(std::ostream& output, const TeamCommand& command) {
+        for (int i = 0; i < NUM_OF_ROBOTS_PER_TEAM; i++) {
+            output << "ROBOT " << i << ":" << std::endl;
+            output << command.robot_command[i] << std::endl;
+            output << std::endl;
+        }
+
+        return output;
+    }
+}
