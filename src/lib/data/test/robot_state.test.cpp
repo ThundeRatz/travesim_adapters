c27e43ba4adb4bdb400cbf4b5845768ea27f66d5
Felipe Gomes de Melo <felipegmelo.42@gmail.com>
2021-04-14 10:15:39 -0300

    ✅  Update tests w/ convertion functions

--- src/lib/data/test/robot_state.test.cpp
+++ src/lib/data/test/robot_state.test.cpp
@@ -4,28 +4,30 @@
 #include <gtest/gtest.h>
 #include <gazebo_msgs/ModelState.h>
 #include <geometry_msgs/Vector3.h>
+#include <iostream>
 
 #include "travesim_adapters/data/robot_state.hpp"
+#include "travesim_adapters/vision_receiver.hpp"
 
 // Declare a test
 TEST(robot_state_to_model_state, model_name)
 {
     travesim::RobotState robot_state;
 
-    EXPECT_EQ(robot_state.to_ModelState().model_name, "yellow_team/robot_0");
+    EXPECT_EQ(travesim::VisionReceiver::RobotState_to_ModelState(&robot_state).model_name, "yellow_team/robot_0");
 
     robot_state.id = 2;
 
-    EXPECT_EQ(robot_state.to_ModelState().model_name, "yellow_team/robot_2");
+    EXPECT_EQ(travesim::VisionReceiver::RobotState_to_ModelState(&robot_state).model_name, "yellow_team/robot_2");
 
     robot_state.is_yellow = false;
     robot_state.id = 0;
 
-    EXPECT_EQ(robot_state.to_ModelState().model_name, "blue_team/robot_0");
+    EXPECT_EQ(travesim::VisionReceiver::RobotState_to_ModelState(&robot_state).model_name, "blue_team/robot_0");
 
     robot_state.id = 1;
 
-    EXPECT_EQ(robot_state.to_ModelState().model_name, "blue_team/robot_1");
+    EXPECT_EQ(travesim::VisionReceiver::RobotState_to_ModelState(&robot_state).model_name, "blue_team/robot_1");
 }
 
 TEST(robot_state_to_model_state, angle_conversion)
@@ -33,16 +35,17 @@ TEST(robot_state_to_model_state, angle_conversion)
     travesim::RobotState robot_state;
     robot_state.angular_position = -M_PI/3;
 
-    gazebo_msgs::ModelState model_state = robot_state.to_ModelState();
+    gazebo_msgs::ModelState model_state = travesim::VisionReceiver::RobotState_to_ModelState(&robot_state);
 
-    EXPECT_DOUBLE_EQ(travesim::RobotState(&model_state).angular_position, robot_state.angular_position);
+    EXPECT_DOUBLE_EQ(travesim::VisionReceiver::ModelState_to_RobotState(&model_state).angular_position,
+                     robot_state.angular_position);
 }
 
 TEST(vector2d_to_vector3, convert_from_vector2d)
 {
     travesim::Vector2D vector2d(2, 4);
 
-    geometry_msgs::Vector3 vector3 = vector2d.to_Vector3();
+    geometry_msgs::Vector3 vector3 = travesim::VisionReceiver::Vector2D_to_Vector3(&vector2d);
 
     EXPECT_DOUBLE_EQ(vector3.x, 2);
     EXPECT_DOUBLE_EQ(vector3.y, 4);
@@ -55,7 +58,7 @@ TEST(vector2d_to_vector3, convert_from_vector3)
     vector3.x = 3;
     vector3.y = -7;
 
-    travesim::Vector2D vector2d(&vector3);
+    travesim::Vector2D vector2d = travesim::VisionReceiver::Vector3_to_Vector2D(&vector3);
 
     EXPECT_DOUBLE_EQ(vector2d.x, 3);
     EXPECT_DOUBLE_EQ(vector2d.y, -7);
