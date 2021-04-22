f21ea34ab379a4616746c351a62f86d9d2610f73
LucasHaug <lucas.haug@hotmail.com>
2021-04-22 20:17:48 -0300

    ✨ Add general udp sender class

--- include/travesim_adapters/udp/multicast_sender.hpp
+++ include/travesim_adapters/udp/multicast_sender.hpp
@@ -11,8 +11,7 @@
  * @copyright MIT License - Copyright (c) 2021 ThundeRatz
  */
 
-#include <boost/asio.hpp>
-#include <string>
+#include "travesim_adapters/udp/sender.hpp"
 
 #ifndef __MULTICAST_SENDER_H__
 #define __MULTICAST_SENDER_H__
@@ -22,7 +21,7 @@ namespace udp {
 /**
  * @brief Sender class using UDP in multicast mode
  */
-class MulticastSender {
+class MulticastSender : public Sender {
     public:
         /**
          * @brief Construct a new Multicast Sender object
@@ -31,26 +30,6 @@ class MulticastSender {
          * @param multicast_port Multicas group port
          */
         MulticastSender(const std::string multicast_address, const short multicast_port);
-
-        /**
-         * @brief Destroy the Multicast Sender object
-         */
-        ~MulticastSender();
-
-        /**
-         * @brief Send data using UDP
-         *
-         * @param buffer Buffer to be sent
-         * @param buffer_size Size of the buffer to be sent
-         *
-         * @return size_t Number of bytes sent
-         */
-        size_t send(const char* buffer, const size_t buffer_size);
-
-    private:
-        boost::asio::io_context io_context;      /**< boost/asio I/O execution context */
-        boost::asio::ip::udp::socket* socket;    /**< Network socket */
-        boost::asio::ip::udp::endpoint endpoint; /**< Multicast address and port pair */
 };
 }  // namespace udp
 }  // namespace travesim
---
+++ include/travesim_adapters/udp/sender.hpp
@@ -0,0 +1,58 @@
+/**
+ * @file sender.hpp
+ *
+ * @author Lucas Haug <lucas.haug@thuneratz.org>
+ * @author Lucas Schneider <lucas.schneider@thuneratz.org>
+
+ * @brief Send data using UDP
+ *
+ * @date 04/2021
+ *
+ * @copyright MIT License - Copyright (c) 2021 ThundeRatz
+ */
+
+#include <boost/asio.hpp>
+#include <string>
+
+#ifndef __SENDER_H__
+#define __SENDER_H__
+
+namespace travesim {
+namespace udp {
+/**
+ * @brief Sender class using UDP
+ */
+class Sender {
+    public:
+        /**
+         * @brief Construct a new Sender object
+         *
+         * @param receiver_address Address where to send data
+         * @param receiver_port Port where to send data
+         */
+        Sender(const std::string receiver_address, const short receiver_port);
+
+        /**
+         * @brief Destroy the Sender object
+         */
+        ~Sender();
+
+        /**
+         * @brief Send data using UDP
+         *
+         * @param buffer Buffer to be sent
+         * @param buffer_size Size of the buffer to be sent
+         *
+         * @return size_t Number of bytes sent
+         */
+        size_t send(const char* buffer, const size_t buffer_size);
+
+    protected:
+        boost::asio::io_context io_context;      /**< boost/asio I/O execution context */
+        boost::asio::ip::udp::socket* socket;    /**< Network socket */
+        boost::asio::ip::udp::endpoint endpoint; /**< Multicast address and port pair */
+};
+}  // namespace udp
+}  // namespace travesim
+
+#endif // __SENDER_H__
