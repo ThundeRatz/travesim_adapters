3acc466f49ad6eaafc59a675710b827e4c9bbc96
LucasHaug <lucas.haug@hotmail.com>
2021-04-28 13:53:46 -0300

    ✅ Add team receiver example

---
+++ examples/protobuf/team_receiver_example.cpp
@@ -0,0 +1,59 @@
+/**
+ * @file vision_receiver_example.cpp
+ *
+ * @author Lucas Haug <lucas.haug@thuneratz.org>
+ * @author Lucas Schneider <lucas.schneider@thuneratz.org>
+ * @author Felipe Gomes de Melo <felipe.gomes@thuneratz.org>
+ *
+ * @brief Example to read data published from vision adapter
+ *
+ * @date 04/2021
+ *
+ * @copyright MIT License - Copyright (c) 2021 ThundeRatz
+ */
+
+#include <iostream>
+#include <boost/asio.hpp>
+
+#include "travesim_adapters/protobuf/team_receiver.hpp"
+
+/*****************************************
+ * Private Constants
+ *****************************************/
+
+#define BUFFER_SIZE 1024U
+#define CLEAR_TERMINAL "\033[2J\033[H"
+
+/*****************************************
+ * Main Function
+ *****************************************/
+
+int main(int argc, char* argv[]) {
+    const std::string receiver_address = "127.0.0.1";
+    const short receiver_port = 20011;
+
+    travesim::TeamCommand team_yellow_cmd;
+
+    try {
+        boost::asio::io_context io_context;
+        boost::asio::steady_timer my_timer(io_context);
+        travesim::proto::TeamReceiver yellow_receiver(receiver_address, receiver_port, true);
+
+        size_t data_size = 0;
+
+        for (long int i = 0;; i++) {
+            bool received_new_msg = yellow_receiver.receive(&team_yellow_cmd);
+
+            if (received_new_msg) {
+                std::cout << team_yellow_cmd << CLEAR_TERMINAL;
+            }
+
+            my_timer.expires_after(std::chrono::milliseconds(1));
+            my_timer.wait();
+        }
+    } catch (std::exception& e) {
+        std::cerr << "Exception: " << e.what() << "\n";
+    }
+
+    return 0;
+}
