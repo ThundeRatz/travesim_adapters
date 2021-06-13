5e82d80f2309ec5b30b58127abdc266685d9e332
LucasHaug <lucas.haug@hotmail.com>
2021-06-13 01:17:25 -0300

    ✨ Add configurers utils

---
+++ include/travesim_adapters/configurers/configurers_utils.hpp
@@ -0,0 +1,47 @@
+/**
+ * @file configurers_utils.hpp
+ *
+ * @author Lucas Haug <lucas.haug@thuneratz.org>
+ *
+ * @brief Configurers utilitary funtions
+ *
+ * @date 06/2021
+ *
+ * @copyright MIT License - Copyright (c) 2021 ThundeRatz
+ *
+ */
+
+/*****************************************
+ * Public Funtions Prototypes
+ *****************************************/
+
+namespace travesim {
+/**
+ * @brief Converts a IPv4 in a string to a array of unsigned integers,
+ *        where the most significant byte of the IP address is in the
+ *        position 0 and the last significant in the position 3.
+ *
+ * @note Expects IPv4 in the format "x.y.z.w",
+ *       where x, y, z and w are numbers.
+ *
+ * @param ip_string IPv4 string to be converted
+ * @param ip_uint Pointer where to store the converted IPv4
+ *
+ * @return true if the conversion was successful, false otherwise
+ */
+bool ipv4_string_to_uint(std::string ip_string, uint* ip_uint);
+
+/**
+ * @brief Checks if a IP is valid and is in the specified range
+ *
+ * @note Expects IPv4 in the format "x.y.z.w",
+ *       where x, y, z and w are numbers.
+ *
+ * @param ip IP string to be converted
+ * @param min_ip Minimum IP interval value
+ * @param max_ip Maximum IP interval value
+ *
+ * @return true if the IP is valid, false otherwise
+ */
+bool check_valid_ip(std::string ip, std::string min_ip, std::string max_ip);
+}  // namespace travesim
---
+++ src/lib/configurers/configurers_utils.cpp
@@ -0,0 +1,106 @@
+/**
+ * @file configurers_utils.cpp
+ *
+ * @author Lucas Haug <lucas.haug@thuneratz.org>
+ *
+ * @brief Configurers utilitary funtions
+ *
+ * @date 06/2021
+ *
+ * @copyright MIT License - Copyright (c) 2021 ThundeRatz
+ *
+ */
+
+/*****************************************
+ * Private Constants
+ *****************************************/
+
+#define IPV4_NUM_OF_BYTES 4U
+
+/*****************************************
+ * Public Funtions Bodies Definitions
+ *****************************************/
+
+namespace travesim {
+bool ipv4_string_to_uint(std::string ip_string, uint* ip_uint) {
+    std::stringstream ip_stream(ip_string);
+
+    char dot;
+
+    if (ip_stream.rdbuf()->in_avail() == 0) {
+        return false;
+    } else {
+        ip_stream >> ip_uint[0];
+
+        if (ip_stream.fail()) {
+            return false;
+        }
+    }
+
+    for (int i = 1; i < IPV4_NUM_OF_BYTES; i++) {
+        if (ip_stream.rdbuf()->in_avail() == 0) {
+            return false;
+        } else {
+            ip_stream >> dot;
+
+            if (ip_stream.fail()) {
+                return false;
+            }
+
+            if (dot != '.') {
+                return false;
+            }
+        }
+
+        if (ip_stream.rdbuf()->in_avail() == 0) {
+            return false;
+        } else {
+            ip_stream >> ip_uint[i];
+
+            if (ip_stream.fail()) {
+                return false;
+            }
+        }
+    }
+
+    if (ip_stream.rdbuf()->in_avail() != 0) {
+        return false;
+    }
+
+    return true;
+}
+
+bool check_valid_ip(std::string ip, std::string min_ip, std::string max_ip) {
+    uint ip_uint[IPV4_NUM_OF_BYTES] = {0};
+    uint min_ip_uint[IPV4_NUM_OF_BYTES] = {0};
+    uint max_ip_uint[IPV4_NUM_OF_BYTES] = {0};
+
+    if (!ipv4_string_to_uint(ip, ip_uint)) {
+        return false;
+    }
+
+    if (!ipv4_string_to_uint(min_ip, min_ip_uint)) {
+        return false;
+    }
+
+    if (!ipv4_string_to_uint(max_ip, max_ip_uint)) {
+        return false;
+    }
+
+    for (int i = 0; i < IPV4_NUM_OF_BYTES; i++) {
+        if (ip_uint[i] > 0xFF) {
+            return false;
+        }
+
+        if (ip_uint[i] > max_ip_uint[i]) {
+            return false;
+        }
+
+        if (ip_uint[i] < min_ip_uint[i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+}
